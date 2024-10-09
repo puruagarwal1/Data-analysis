2e7a27d09d0067f48104d8eb89a1151d8c535f1c
Kapish Jain <61595130+omjain1@users.noreply.github.com>
2024-10-09 10:16:20 +0530

    Update and rename numpy to numpy.py

--- numpy.py
+++ numpy.py
@@ -5,6 +5,8 @@ print("Matrix b : \n", b)
  
 a = np.empty([2, 2], dtype = int)
 print("\nMatrix a : \n", a)
+
+print(a*b) # matrix multiplication
  
 c = np.empty([3, 3])
 print("\nMatrix c : \n", c)
